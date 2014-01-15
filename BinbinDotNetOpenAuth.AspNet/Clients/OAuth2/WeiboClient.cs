fba6f411290441255d6caac0980364252bfa4caa
lotosbin <lotosbin@gmail.com>
2014-01-15 21:05:57 +0800

    format code

--- BinbinDotNetOpenAuth.AspNet/Clients/OAuth2/WeiboClient.cs
+++ BinbinDotNetOpenAuth.AspNet/Clients/OAuth2/WeiboClient.cs
@@ -56,7 +56,11 @@ namespace BinbinDotNetOpenAuth.AspNet
         /// </summary>
         /// <param name="clientId">The Google Client Id</param>
         /// <param name="clientSecret">The Google Client Secret</param>
-        public WeiboClient(string clientId, string clientSecret) : this(clientId, clientSecret, new[] {"email"})
+        public WeiboClient(string clientId, string clientSecret)
+            : this(clientId, clientSecret, new[]
+                                           {
+                                               "email"
+                                           })
         {
         }
 
@@ -66,52 +70,81 @@ namespace BinbinDotNetOpenAuth.AspNet
         /// <param name="clientId">The Google Client Id</param>
         /// <param name="clientSecret">The Google Client Secret</param>
         /// <param name="requestedScopes">One or more requested scopes, passed without the base URI.</param>
-        public WeiboClient(string clientId, string clientSecret, params string[] requestedScopes) : base("weibo")
+        public WeiboClient(string clientId, string clientSecret, params string[] requestedScopes)
+            : base("weibo")
         {
             if (string.IsNullOrWhiteSpace(clientId))
+            {
                 throw new ArgumentNullException("clientId");
+            }
 
             if (string.IsNullOrWhiteSpace(clientSecret))
+            {
                 throw new ArgumentNullException("clientSecret");
+            }
 
             if (requestedScopes == null)
+            {
                 throw new ArgumentNullException("requestedScopes");
+            }
 
             if (requestedScopes.Length == 0)
+            {
                 throw new ArgumentException("One or more scopes must be requested.", "requestedScopes");
+            }
 
-            _clientId = clientId;
-            _clientSecret = clientSecret;
-            _requestedScopes = requestedScopes;
+            this._clientId = clientId;
+            this._clientSecret = clientSecret;
+            this._requestedScopes = requestedScopes;
         }
 
         protected override Uri GetServiceLoginUrl(Uri returnUrl)
         {
-            IEnumerable<string> scopes = _requestedScopes;
+            IEnumerable<string> scopes = this._requestedScopes;
             string state = string.IsNullOrEmpty(returnUrl.Query) ? string.Empty : returnUrl.Query.Substring(1);
 
-            return UriHelper.BuildUri(AuthorizationEndpoint, new NameValueCollection {{"response_type", "code"}, {"client_id", _clientId}, {"scope", string.Join(" ", scopes)}, {"redirect_uri", returnUrl.GetLeftPart(UriPartial.Path)}, {"state", state},});
+            return UriHelper.BuildUri(AuthorizationEndpoint, new NameValueCollection
+                                                             {
+                                                                 {"response_type", "code"},
+                                                                 {"client_id", this._clientId},
+                                                                 {"scope", string.Join(" ", scopes)},
+                                                                 {"redirect_uri", returnUrl.GetLeftPart(UriPartial.Path)},
+                                                                 {"state", state},
+                                                             });
         }
 
         protected override IDictionary<string, string> GetUserData(string accessToken)
         {
             var uid = (string) HttpContext.Current.Session["uid"];
-            Uri uri = UriHelper.BuildUri(UserInfoEndpoint, new NameValueCollection {{"access_token", accessToken}, {"uid", uid}});
+            Uri uri = UriHelper.BuildUri(UserInfoEndpoint, new NameValueCollection
+                                                           {
+                                                               {"access_token", accessToken},
+                                                               {"uid", uid}
+                                                           });
 
             var webRequest = (HttpWebRequest) WebRequest.Create(uri);
 
             using (WebResponse webResponse = webRequest.GetResponse())
-            using (Stream stream = webResponse.GetResponseStream())
             {
-                if (stream == null)
-                    return null;
-
-                using (var textReader = new StreamReader(stream))
+                using (Stream stream = webResponse.GetResponseStream())
                 {
-                    string json = textReader.ReadToEnd();
-                    var user = JsonConvert.DeserializeObject<WeiboUserData>(json);
-                    var extraData = new Dictionary<string, string> {{"id", user.id}, {"name", user.name}, {"screen_name", user.screen_name}};
-                    return extraData;
+                    if (stream == null)
+                    {
+                        return null;
+                    }
+
+                    using (var textReader = new StreamReader(stream))
+                    {
+                        string json = textReader.ReadToEnd();
+                        var user = JsonConvert.DeserializeObject<WeiboUserData>(json);
+                        var extraData = new Dictionary<string, string>
+                                        {
+                                            {"id", user.id},
+                                            {"name", user.name},
+                                            {"screen_name", user.screen_name}
+                                        };
+                        return extraData;
+                    }
                 }
             }
         }
@@ -119,7 +152,14 @@ namespace BinbinDotNetOpenAuth.AspNet
         protected override string QueryAccessToken(Uri returnUrl, string authorizationCode)
         {
             NameValueCollection postData = HttpUtility.ParseQueryString(string.Empty);
-            postData.Add(new NameValueCollection {{"grant_type", "authorization_code"}, {"code", authorizationCode}, {"client_id", _clientId}, {"client_secret", _clientSecret}, {"redirect_uri", returnUrl.GetLeftPart(UriPartial.Path)},});
+            postData.Add(new NameValueCollection
+                         {
+                             {"grant_type", "authorization_code"},
+                             {"code", authorizationCode},
+                             {"client_id", this._clientId},
+                             {"client_secret", this._clientSecret},
+                             {"redirect_uri", returnUrl.GetLeftPart(UriPartial.Path)},
+                         });
 
             var webRequest = (HttpWebRequest) WebRequest.Create(TokenEndpoint);
 
@@ -127,14 +167,20 @@ namespace BinbinDotNetOpenAuth.AspNet
             webRequest.ContentType = "application/x-www-form-urlencoded";
 
             using (Stream s = webRequest.GetRequestStream())
-            using (var sw = new StreamWriter(s))
-                sw.Write(postData.ToString());
+            {
+                using (var sw = new StreamWriter(s))
+                {
+                    sw.Write(postData.ToString());
+                }
+            }
 
             using (WebResponse webResponse = webRequest.GetResponse())
             {
                 Stream responseStream = webResponse.GetResponseStream();
                 if (responseStream == null)
+                {
                     return null;
+                }
 
                 using (var reader = new StreamReader(responseStream))
                 {
