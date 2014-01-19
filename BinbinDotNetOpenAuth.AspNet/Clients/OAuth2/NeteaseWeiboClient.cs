9ee25326a6d5a625d9a2e687ba16f89c6ae5dca6
lotosbin <lotosbin@gmail.com>
2014-01-19 16:49:18 +0800

    netease oauth

---
+++ BinbinDotNetOpenAuth.AspNet/Clients/OAuth2/NeteaseWeiboClient.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.Serialization;
+using DotNetOpenAuth.AspNet.Clients;
+using log4net;
+using Newtonsoft.Json;
+
+namespace BinbinDotNetOpenAuth.AspNet.Clients
+{
+    /// <summary>
+    ///     网易
+    /// </summary>
+    public class NeteaseWeiboClient : OAuth2Client
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof (NeteaseWeiboClient));
+
+        /// <summary>
+        ///     Creates a new Google OAuth2 Client, requesting the default "userinfo.profile" and "userinfo.email" scopes.
+        /// </summary>
+        /// <param name="clientId">The Google Client Id</param>
+        /// <param name="clientSecret">The Google Client Secret</param>
+        public NeteaseWeiboClient(string clientId, string clientSecret)
+            : this(clientId, clientSecret, new[]
+                                           {
+                                               "basic"
+                                           })
+        {
+        }
+
+        /// <summary>
+        ///     Creates a new Google OAuth2 client.
+        /// </summary>
+        /// <param name="clientId">The Google Client Id</param>
+        /// <param name="clientSecret">The Google Client Secret</param>
+        /// <param name="requestedScopes">One or more requested scopes, passed without the base URI.</param>
+        public NeteaseWeiboClient(string clientId, string clientSecret, params string[] requestedScopes)
+            : base("neteaseweibo")
+        {
+            if (string.IsNullOrWhiteSpace(clientId))
+            {
+                throw new ArgumentNullException("clientId");
+            }
+
+            if (string.IsNullOrWhiteSpace(clientSecret))
+            {
+                throw new ArgumentNullException("clientSecret");
+            }
+
+            if (requestedScopes == null)
+            {
+                throw new ArgumentNullException("requestedScopes");
+            }
+
+            //if (requestedScopes.Length == 0)
+            //{
+            //    throw new ArgumentException("One or more scopes must be requested.", "requestedScopes");
+            //}
+
+            this._clientId = clientId;
+            this._clientSecret = clientSecret;
+            this._requestedScopes = requestedScopes;
+        }
+
+
+        protected override Uri GetServiceLoginUrl(Uri returnUrl)
+        {
+            log.Info("GetServiceLoginUrl");
+            IEnumerable<string> scopes = this._requestedScopes;
+            string state = string.IsNullOrEmpty(returnUrl.Query) ? string.Empty : returnUrl.Query.Substring(1);
+
+            var collection = new NameValueCollection
+                             {
+                                 {"response_type", "code"},
+                                 {"client_id", this._clientId},
+                                 {"scope", string.Join(" ", scopes)},
+                                 {"redirect_uri", returnUrl.GetLeftPart(UriPartial.Path)},
+                                 {"state", state},
+                             };
+            return UriHelper.BuildUri(AuthorizationEndpoint, collection);
+        }
+
+        protected override IDictionary<string, string> GetUserData(string accessToken)
+        {
+            log.Info("GetUserData");
+            //var uid = (string) HttpContext.Current.Session["uid"];
+            var collection = new NameValueCollection
+                             {
+                                 {"access_token", accessToken},
+                             };
+            string json = UriHelper.OAuthGet(UserInfoEndpoint, collection);
+            log.Info("response:" + json);
+            var result = JsonConvert.DeserializeObject<GetUserDataResult>(json);
+            var extraData = new Dictionary<string, string>
+                            {
+                                {"id", result.id},
+                                {"name", result.name},
+                            };
+            return extraData;
+        }
+
+        protected override string QueryAccessToken(Uri returnUrl, string authorizationCode)
+        {
+            log.Info("QueryAccessToken(authcode:" + authorizationCode + ")");
+            var valueCollection = new NameValueCollection
+                                  {
+                                      {"grant_type", "authorization_code"},
+                                      {"code", authorizationCode},
+                                      {"client_id", this._clientId},
+                                      {"client_secret", this._clientSecret},
+                                      {"redirect_uri", returnUrl.GetLeftPart(UriPartial.Path)},
+                                  };
+            string json = UriHelper.OAuthPost(TokenEndpoint, valueCollection);
+            log.Info("response:" + json);
+            if (json == null)
+            {
+                return null;
+            }
+            var data = JsonConvert.DeserializeObject<QueryAccessTokenResponseData>(json);
+            string accessToken = data.access_token;
+
+            //HttpContext.Current.Session["uid"] = data.open_id;
+            return accessToken;
+        }
+
+        #region Constants and Fields
+
+        /// <summary>
+        ///     The authorization endpoint.
+        /// </summary>
+        private const string AuthorizationEndpoint = "http://api.t.163.com/oauth2/authenticate";
+
+        /// <summary>
+        ///     The token endpoint.
+        /// </summary>
+        private const string TokenEndpoint = "http://api.t.163.com/oauth2/access_token";
+
+        /// <summary>
+        ///     The user info endpoint.
+        /// </summary>
+        private const string UserInfoEndpoint = "https://api.t.163.com/users/show.json";
+
+        /// <summary>
+        ///     The _app id.
+        /// </summary>
+        protected readonly string _clientId;
+
+        /// <summary>
+        ///     The _app secret.
+        /// </summary>
+        protected readonly string _clientSecret;
+
+        /// <summary>
+        ///     The requested scopes.
+        /// </summary>
+        private readonly string[] _requestedScopes;
+
+        #endregion
+
+        [DataContract]
+        [Serializable]
+        public class GetUserDataResult
+        {
+            /// <summary>
+            /// </summary>
+            [DataMember]
+            public string id { get; set; }
+
+            /// <summary>
+            ///     状态描述
+            /// </summary>
+            [DataMember]
+            public string name { get; set; }
+        }
+
+        [DataContract]
+        [Serializable]
+        public class QueryAccessTokenResponseData
+        {
+            [DataMember]
+            public string access_token { get; set; }
+        }
+    }
+}
